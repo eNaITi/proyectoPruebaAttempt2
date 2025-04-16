940cbcdbdc1ea803b586b40f23928a4c76ce8a9c
martin <martin@gmail.com>
2025-04-16 17:37:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/proyectoprueba/activityShow.kt
@@ -0,0 +1,23 @@
+package com.example.proyectoprueba
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activityShow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_show)
+
+
+
+
+
+
+
+
+    }
+}
